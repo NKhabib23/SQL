73a0b67e92d75ca9bc8579e6669782286e35e2c3
NKhabib23 <nkhabib18@gmail.com>
2025-04-07 15:38:57 +0500

    Create hw.sql

---
+++ lesson-1/homework/hw.sql
@@ -0,0 +1,17 @@
+--1. Define the following terms: data, database, relational database, and table.
+
+--Datation means Information. 
+--Database means wherehouse which full of information.
+--relational datebase means a few wherehouses which have their own wherehouse.
+--Table is a shape that made up of columns and rows.
+
+--2. List five key features of SQL Server.
+--1-Select
+--2-Insert
+--3-Delate
+--4-Update
+--5-Drop
+
+--3. What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+--1-Windows
+--2-SQL Server
